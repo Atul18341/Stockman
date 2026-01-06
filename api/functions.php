adcde8d986f2a8fb74f886bc96738711b1908539
ATUL KUMAR <58041005+Atul18341@users.noreply.github.com>
2026-01-06 11:20:55 +0530

    session setups deleted

--- api/functions.php
+++ api/functions.php
@@ -2,9 +2,6 @@
 // functions.php
 
 // 1. Secure Session Setup
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_only_cookies', 1);
-ini_set('session.cookie_samesite', 'Strict');
 
 session_start();
 
